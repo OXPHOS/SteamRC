4166910ba2f7f9678710f06e99821e94c48b1e2d
OXPHOS <engelzora@gmail.com>
2017-06-09 00:32:50 -0400

    Update app data retrieval script

--- gamedata_crawler.py
+++ gamedata_crawler.py
@@ -2,9 +2,8 @@
 # Detailed information of each game can be retrieved at http://store.steampowered.com/api/appdetails?appids=10
 
 import json
+import os
 import requests
-import re
-
 
 HEADER = {
     'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.361',
@@ -17,16 +16,26 @@ gameListUrl = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
 gameListResp = requests.get(gameListUrl, HEADER) #html
 gameListJson = gameListResp.json()
 
+try:
+    os.remove('./rawdata/apps_detail.txt')
+except OSError:
+    pass
+
 appidList = []
 for apps in gameListJson['applist']['apps']:
     appidList.append(apps['appid'])
-    
+
+#print len(appidList) #42026
 
 
-for _ in appidList[0:10]:
-    gameListUrl = "http://store.steampowered.com/api/appdetails?appids=%s"%_
-    gameListResp = requests.get(gameListUrl, HEADER) #html
-    gameListJson = gameListResp.json()
+for _ in appidList:
+    gameUrl = "http://store.steampowered.com/api/appdetails?appids=%s"%_
+    gameResp = requests.get(gameUrl, HEADER) #html
+    gameJson = gameResp.json()
     
-    with open('./rawdata/app_detail.txt', 'a') as f:
-        json.dump(gameListJson, f)
+    if (gameJson[str(_)]).get('success') == False:
+        continue
+    else:
+    #    print gameJson
+        with open('./rawdata/apps_detail.txt', 'a+') as f:
+            json.dump(gameJson, f)
